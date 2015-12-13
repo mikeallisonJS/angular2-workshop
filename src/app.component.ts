bb367b8637174279645da6d45cd9357c5cbb927d
Mike Allison <mike@mikeallisononline.com>
2015-12-13 13:01:57 -0600

    update to angular 0.53

--- src/app.component.ts
+++ src/app.component.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'zone.js';
 import {bootstrap} from 'angular2/bootstrap'
 import {Component} from 'angular2/core';
+import {FORM_DIRECTIVES} from 'angular2/common'
 import Hero from './hero';
 
 @Component({
@@ -10,7 +11,7 @@ import Hero from './hero';
 	<h1>{{title}}</h1>
 	<h2>{{hero.name}} details!</h2>
 	<div><label>id: </label>{{hero.id}}</div>
-	<div><label>name: </label><input [(ng-model)]="hero.name" placeholder="name"></div>
+	<div><label>name: </label><input [(ngModel)]="hero.name" placeholder="name"></div>
 	`,
 	directives: [FORM_DIRECTIVES]
 })
